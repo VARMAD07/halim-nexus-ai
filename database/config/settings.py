b0a57193ccbdda11ab763fc84d059160f1e62b78
Mohammad Hammad Faridi <hammadfardi713@gmail.com>
2026-05-18 09:02:03 +0530

    Remove leaked API key from database config

--- database/config/settings.py
+++ database/config/settings.py
@@ -1,6 +1,6 @@
 import os
 class Settings:
 
-    GEMINI_API_KEY = os.getenv("AIzaSyBnFQ4DijqgmQ1IehAf0k_CDVSjOqdE1WY", "fallback_value")
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "fallback_value")
 
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "fallback_value")
